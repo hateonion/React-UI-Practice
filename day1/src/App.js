38a79e23a36714529094d4cf1c4bb43e2bf7011d
hateonion <ouyang_yingjie@outlook.com>
2017-09-12 23:55:05 +0800

    Implement button animation and focus icon color change

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      isFocus: false
+    };
+
+    this.handleUserNameInput = this.handleUserNameInput.bind(this);
+    this.handleUserEmailInput = this.handleUserEmailInput.bind(this);
+    this.handleUserPasswordInput = this.handleUserPasswordInput.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  handleUserNameInput(e) {
+    this.setState({username: e.target.value});
+  }
+
+  handleUserEmailInput(e) {
+    this.setState({email: e.target.value});
+  }
+
+  handleUserPasswordInput(e) {
+    this.setState({password: e.target.value});
+  }
+
+  handleFocus() {
+    this.setState({isFocus: true});
+  }
+
+  handleBlur() {
+    this.setState({isFocus: false});
+  }
+
   render() {
+    const {username, email, password, isFocus} = this.state;
     return (
       <div className="login-container">
         <div className="login-modal">
           <div className="login-panel">
             <ul>
               <li>
-                <span className="fa fa-address-card fa-fw"></span>
-                <input className="user-name" name="" type="text" value="" placeholder="john smith"/>
+                <span className={`fa fa-address-card fa-fw ${isFocus ? 'input__focus' : ''}`}></span>
+                <input className="user-name" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleUserNameInput} name="username" type="text" value={username} placeholder="john smith"/>
               </li>
               <li>
                 <span className="fa fa-envelope fa-fw"></span>
-                <input className="user-email" name="" type="text" value="" placeholder="john@smith.con"/>
+                <input className="user-email" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleUserEmailInput} name="email" type="text" value={email} placeholder="john@smith.con"/>
               </li>
               <li>
                 <span className="fa fa-unlock-alt fa-fw"></span>
-                <input className="user-password" name="" type="text" value="" placeholder="password"/>
+                <input className="user-password" onFocus={this.handleFocus} onBlur={this.handleBlur} required onChange={this.handleUserPasswordInput} name="password" type="text" value={password} placeholder="password"/>
               </li>
             </ul>
             <button className="login-button">
@@ -31,5 +70,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;

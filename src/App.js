a935951eb42432cef1957214068b93e5722a670c
hateonion <ouyang_yingjie@outlook.com>
2017-11-07 22:24:58 +0800

    Day-1 add animation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import {
-  Route,
-  BrowserRouter as Router,
-  Link
+    Route,
+    BrowserRouter as Router,
+    Link
 } from 'react-router-dom';
 import SignUpForm from './components/signupForm';
 import './App.css';
 
 const Home = () => (
-        <ul>
-          <li><Link to='/signupForm'>Day1</Link></li>
-        </ul>
-) 
+    <ul>
+        <li><Link to='/signupForm'>Day1</Link></li>
+    </ul>
+);
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div id='app'>
-          <Route path='/signupForm' component={SignUpForm} />
-          <Route exact path='/' component={Home}/>
-        </div>
-      </Router>
+    render() {
+        return (
+            <Router>
+                <div id='app'>
+                    <Route path='/signupForm' component={SignUpForm} />
+                    <Route exact path='/' component={Home}/>
+                </div>
+            </Router>
 
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/signupForm/index.js
+++ src/components/signupForm/index.js
@@ -3,70 +3,60 @@ import './signupForm.css';
 
 export default class SignUpForm extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      username: '',
-      password: '',
-      email: '',
-      isFocus: false,
-    };
+        this.state = {
+            username: '',
+            password: '',
+            email: '',
+            isFocus: false,
+        };
 
-    this.handleUserNameInput = this.handleUserNameInput.bind(this);
-    this.handleUserEmailInput = this.handleUserEmailInput.bind(this);
-    this.handleUserPasswordInput = this.handleUserPasswordInput.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-  }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleFocus = this.handleFocus.bind(this);
+        this.handleBlur = this.handleBlur.bind(this);
+    }
 
-  handleUserNameInput(e) {
-    this.setState({username: e.target.value});
-  }
+    handleInput(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
 
-  handleUserEmailInput(e) {
-    this.setState({email: e.target.value});
-  }
+    handleFocus() {
+        this.setState({isFocus: true});
+    }
 
-  handleUserPasswordInput(e) {
-    this.setState({password: e.target.value});
-  }
+    handleBlur() {
+        this.setState({isFocus: false});
+    }
 
-  handleFocus() {
-    this.setState({isFocus: true});
-  }
-
-  handleBlur() {
-    this.setState({isFocus: false});
-  }
-
-  render() {
-    const {username, email, password, isFocus} = this.state;
-    return (
-      <div className="login-container">
-        <div className="login-modal">
-          <form className="login-panel">
-            <ul>
-              <li>
-                <span className={`fa fa-address-card fa-fw ${isFocus ? 'input__focus' : ''}`}></span>
-                <input className="user-name" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleUserNameInput} name="username" type="text" value={username} placeholder="john smith"/>
-              </li>
-              <li>
-                <span className="fa fa-envelope fa-fw"></span>
-                <input className="user-email" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleUserEmailInput} name="email" type="email" value={email} placeholder="john@smith.con"/>
-              </li>
-              <li>
-                <span className="fa fa-unlock-alt fa-fw"></span>
-                <input className="user-password" onFocus={this.handleFocus} onBlur={this.handleBlur} required onChange={this.handleUserPasswordInput} name="password" type="password" value={password} placeholder="password"/>
-              </li>
-            </ul>
-            <button className="login-button" type="submit">
-              <span>LOG IN</span>
-              <span>></span>
-            </button>
-          </form>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {username, email, password } = this.state;
+        return (
+            <div className="login-container">
+                <div className="login-modal">
+                    <form className="login-panel">
+                        <ul>
+                            <li>
+                                <input className="user-name" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleInput} name="username" type="text" value={username} placeholder="john smith"/>
+                                <label className="fa fa-address-card fa-fw"></label>
+                            </li>
+                            <li>
+                                <input className="user-email" required onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleInput} name="email" type="email" value={email} placeholder="john@smith.con"/>
+                                <label className="fa fa-envelope fa-fw"></label>
+                            </li>
+                            <li>
+                                <input className="user-password" onFocus={this.handleFocus} onBlur={this.handleBlur} required onChange={this.handleInput} name="password" type="password" value={password} placeholder="password"/>
+                                <label className="fa fa-unlock-alt fa-fw"></label>
+                            </li>
+                        </ul>
+                        <button className="login-button" type="submit">
+                            <span>LOG IN</span>
+                            <i className="fa fa-angle-right"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }

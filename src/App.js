28a783790cc2dedee6fc0b55a01d58b1cc74e025
hateonion <ouyang_yingjie@outlook.com>
2017-11-07 22:38:26 +0800

    Day-2 add day2 structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
 import {
-    Route,
-    BrowserRouter as Router,
-    Link
+  Route,
+  BrowserRouter as Router,
+  Link
 } from 'react-router-dom';
 import SignUpForm from './components/signupForm';
+import Receipt from './components/receipt';
 import './App.css';
 
 const Home = () => (
-    <ul>
-        <li><Link to='/signupForm'>Day1</Link></li>
-    </ul>
+  <ul>
+    <li><Link to='/signupForm'>Day1</Link></li>
+    <li><Link to='/receipt'>Day2</Link></li>
+  </ul>
 );
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div id='app'>
-                    <Route path='/signupForm' component={SignUpForm} />
-                    <Route exact path='/' component={Home}/>
-                </div>
-            </Router>
+  render() {
+    return (
+      <Router>
+        <div id='app'>
+          <Route path='/signupForm' component={SignUpForm} />
+          <Route path='/receipt' component={Receipt} />
+          <Route exact path='/' component={Home} />
+        </div>
+      </Router>
 
-        );
-    }
+    );
+  }
 }
 
 export default App;

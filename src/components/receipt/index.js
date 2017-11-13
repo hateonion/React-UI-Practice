5c260c8c7f29579ebcb63a5d8768f572b4ecd8f7
hateonion <ouyang_yingjie@outlook.com>
2017-11-13 19:39:46 +0800

    Day2 add css init

--- src/components/receipt/index.js
+++ src/components/receipt/index.js
@@ -24,9 +24,12 @@ class Receipt extends Component {
         <Slider onChange={this.onChange} />
         <div className="receipt-body">
           <div className="receipt-desc">
-            <h2>Coworking Space, South Korea</h2>
-            <p><span>320GBP</span><span> / 1day</span></p>
-            <p>Entire office for 1 person Mon,Aug 22,2016 to Fri,Aug29,2016</p>
+              <h2 className="receipt-title"><span>Coworking Space,</span><span>South Korea</span></h2>
+            <p className="receipt-cost"><span>320GBP</span><span> / 1day</span></p>
+            <p className="receipt-position">
+                <span>Entire office for 1 person </span>
+                <span>Mon,Aug 22,2016 to Fri,Aug29,2016</span>
+            </p>
           </div>
           <div className="receipt-form"></div>
         </div>
